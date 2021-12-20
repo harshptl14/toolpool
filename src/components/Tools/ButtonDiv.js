97f011176168b0f16cae22c3b43d482f4ff504f8
harshptl14 <harshptl714@gmail.com>
2021-12-21 01:24:21 +0530

    reusable button div created

---
+++ src/components/Tools/ButtonDiv.js
@@ -0,0 +1,98 @@
+import React from "react";
+import styled from "styled-components";
+
+const StyledButtonDiv = styled.div`
+  display: flex;
+  width: 100%;
+  flex-wrap: wrap;
+  justify-content: flex-start;
+
+  @media (min-width: 800px) {
+    justify-content: space-between;
+  }
+`;
+
+const ChangeButtonDiv = styled.div`
+  display: flex;
+  width: 100%;
+  flex-wrap: wrap;
+
+  @media (min-width: 800px) {
+    width: 70%;
+    justify-content: flex-start;
+  }
+`;
+const OutputButtonDiv = styled.div`
+  display: flex;
+  width: 100%;
+  flex-wrap: wrap;
+  margin-top: 20px;
+
+  @media (min-width: 800px) {
+    width: 30%;
+    justify-content: flex-end;
+    margin-top: 0px;
+    height: min-content;
+  }
+`;
+
+const StyledButton = styled.button`
+  --submit-color: ${({ theme }) => theme.shade};
+  ${({ theme }) => theme.mixins.smallButton};
+  background-color: ${(props) =>
+    props.type === "submit" ? "var(--submit-color)" : "transparent"};
+  margin-right: 20px;
+  margin-bottom: 20px;
+
+  :hover {
+    background-color: ${(props) =>
+      props.type === "submit" ? "transparent" : "var(--submit-color)"};
+  }
+
+  @media (min-width: 800px) {
+    margin-right: ${(props) => (props.rightpadd === "true" ? "20px" : "0px")};
+    margin-left: ${(props) => (props.rightpadd === "true" ? "0px" : "20px")};
+  }
+`;
+
+const ButtonDiv = ({ filter, finalButtons }) => {
+  return (
+    <StyledButtonDiv>
+      <ChangeButtonDiv>
+        {filter.map(({ title, method, key }) => {
+          return (
+            <StyledButton
+              type="normal"
+              rightpadd="true"
+              key={key}
+              onClick={(e) => {
+                method();
+              }}
+            >
+              {title}
+            </StyledButton>
+          );
+        })}
+      </ChangeButtonDiv>
+
+      <OutputButtonDiv>
+        {finalButtons.map(({ title, method, key, type }) => {
+          return (
+            <StyledButton
+              type={type}
+              rightpadd="false"
+              key={key}
+              onClick={(e) => {
+                method();
+              }}
+            >
+              {title}
+            </StyledButton>
+          );
+        })}
+      </OutputButtonDiv>
+    </StyledButtonDiv>
+  );
+};
+
+export default ButtonDiv;
--- src/components/Tools/TextTools/CaseConverter/caseconverter.js
+++ src/components/Tools/TextTools/CaseConverter/caseconverter.js
@@ -2,25 +2,7 @@ import { useState, useEffect } from "react";
 import "../../../../static/helpers/string_extensions";
 import { copyToClipboard } from "../../../../static/helpers/helperfunctions";
 import styled from "styled-components";
-
-const StyledButton = styled.button`
-  --submit-color: ${({ theme }) => theme.shade};
-  ${({ theme }) => theme.mixins.smallButton};
-  background-color: ${(props) =>
-    props.type === "submit" ? "var(--submit-color)" : "transparent"};
-  margin-right: 20px;
-  margin-bottom: 20px;
-
-  :hover {
-    background-color: ${(props) =>
-      props.type === "submit" ? "transparent" : "var(--submit-color)"};
-  }
-
-  @media (min-width: 800px) {
-    margin-right: ${(props) => (props.rightpadd === "true" ? "20px" : "0px")};
-    margin-left: ${(props) => (props.rightpadd === "true" ? "0px" : "20px")};
-  }
-`;
+import ButtonDiv from "../../ButtonDiv";
 
 const StyledTextArea = styled.textarea`
   width: 100%;
@@ -36,76 +18,56 @@ const StyledTextArea = styled.textarea`
   }
 `;
 
-const ButtonDiv = styled.div`
-  display: flex;
-  width: 100%;
-  flex-wrap: wrap;
-  justify-content: flex-start;
-
-  @media (min-width: 800px) {
-    justify-content: space-between;
-  }
-`;
-
-const ChangeButtonDiv = styled.div`
-  display: flex;
-  width: 100%;
-  flex-wrap: wrap;
-
-  @media (min-width: 800px) {
-    width: 70%;
-    justify-content: flex-start;
-  }
-`;
-const OutputButtonDiv = styled.div`
-  display: flex;
-  width: 100%;
-  flex-wrap: wrap;
-  margin-top: 20px;
-
-  @media (min-width: 800px) {
-    width: 30%;
-    justify-content: flex-end;
-    margin-top: 0px;
-    height: min-content;
-  }
-`;
-
 const CaseConverter = () => {
   const [inputText, setInputText] = useState("");
 
   useEffect(() => {}, [inputText]);
 
-  const caseConverterData = [
+  const filter = [
     {
       key: "1",
       title: "UpperCase",
-      method: inputText.toUpperCase(),
+      method: () => setInputText(inputText.toUpperCase()),
     },
     {
       key: "2",
       title: "LowerCase",
-      method: inputText.toLowerCase(),
+      method: () => setInputText(inputText.toLowerCase()),
     },
     {
       key: "3",
       title: "MixedCase",
-      method: inputText.toMixedCase(),
+      method: () => setInputText(inputText.toMixedCase()),
     },
     {
       key: "4",
       title: "InverseCase",
-      method: inputText.toInverseCase(),
+      method: () => setInputText(inputText.toInverseCase()),
     },
     {
       key: "5",
       title: "SentenceCase",
-      method: inputText.toSentenceCase(),
+      method: () => setInputText(inputText.toSentenceCase()),
     },
     {
       key: "6",
       title: "TitleCase",
-      method: inputText.toTitleCase(),
+      method: () => setInputText(inputText.toTitleCase()),
+    },
+  ];
+
+  const finalButtons = [
+    {
+      key: "7",
+      title: "Reset",
+      method: () => setInputText(""),
+      type: "normal",
+    },
+    {
+      key: "8",
+      title: "Copy",
+      method: () => copyToClipboard(inputText),
+      type: "submit",
     },
   ];
 
@@ -123,45 +85,8 @@ const CaseConverter = () => {
           }}
         ></StyledTextArea>
       </div>
-      <ButtonDiv>
-        <ChangeButtonDiv>
-          {caseConverterData.map(({ title, method, key }) => {
-            return (
-              <StyledButton
-                type="normal"
-                rightpadd="true"
-                key={key}
-                onClick={(e) => {
-                  setInputText(method);
-                }}
-              >
-                {title}
-              </StyledButton>
-            );
-          })}
-        </ChangeButtonDiv>
 
-        <OutputButtonDiv>
-          <StyledButton
-            type="normal"
-            rightpadd="false"
-            onClick={(e) => {
-              setInputText("");
-            }}
-          >
-            Reset
-          </StyledButton>
-          <StyledButton
-            type="submit"
-            rightpadd="false"
-            onClick={(e) => {
-              copyToClipboard(inputText);
-            }}
-          >
-            Copy
-          </StyledButton>
-        </OutputButtonDiv>
-      </ButtonDiv>
+      <ButtonDiv filter={filter} finalButtons={finalButtons} />
     </div>
   );
 };

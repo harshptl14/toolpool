136a04ba9d25c2bf5aa3977733a9583554ce3c88
Yash Paneliya <yashsp1158@gmail.com>
2022-01-19 23:43:44 +0530

    Minor fixes

--- styles/GlobalStyle.js
+++ styles/GlobalStyle.js
@@ -25,20 +25,13 @@ a {
   }
 
   ::selection {
-<<<<<<< HEAD:src/styles/GlobalStyle.js
-=======
     transition: var(--transition);
->>>>>>> 619625bd20db855af2b7da783476e5ccfe709859:styles/GlobalStyle.js
     background-color: ${({ theme }) => theme.shade};
     color: ${({ theme }) => theme.color};
   }
 
   :focus {
-<<<<<<< HEAD:src/styles/GlobalStyle.js
-    outline: 2px dashed ${({ theme }) => theme.shade};
-=======
     outline: 1.8px dashed ${({ theme }) => theme.color};
->>>>>>> 619625bd20db855af2b7da783476e5ccfe709859:styles/GlobalStyle.js
     outline-offset: 3px;
   }
 
@@ -52,21 +45,12 @@ a {
   }
 
   body::-webkit-scrollbar-track {
-<<<<<<< HEAD:src/styles/GlobalStyle.js
-    background: ${({ theme }) => theme.shade};
-=======
     /* background: ${({ theme }) => theme.shade}; */
->>>>>>> 619625bd20db855af2b7da783476e5ccfe709859:styles/GlobalStyle.js
   }
   
   body::-webkit-scrollbar-thumb {
-<<<<<<< HEAD:src/styles/GlobalStyle.js
-    background-color: ${({ theme }) => theme.shade};;
-    border: 3px solid ${({ theme }) => theme.color};;
-=======
     background-color: ${({ theme }) => theme.border};
     /* border: 1px solid ${({ theme }) => theme.color}; */
->>>>>>> 619625bd20db855af2b7da783476e5ccfe709859:styles/GlobalStyle.js
     border-radius: 10px;
   }
 
@@ -112,12 +96,6 @@ a {
     min-height: 100vh;
     display: flex;
     flex-direction: column;
-<<<<<<< HEAD:src/styles/GlobalStyle.js
-    /* display: grid;
-    grid-template-rows: 1fr auto;
-    grid-template-columns: 100%; */
-=======
->>>>>>> 619625bd20db855af2b7da783476e5ccfe709859:styles/GlobalStyle.js
   }
 `;
 

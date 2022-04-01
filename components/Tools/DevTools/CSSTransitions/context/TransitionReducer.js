ed5138b61dc058f4d4125f49462d949536b5b5ca
Yash Paneliya <yashsp1158@gmail.com>
2022-04-01 16:41:53 +0530

    Action handler changes

--- components/Tools/DevTools/CSSTransitions/context/TransitionReducer.js
+++ components/Tools/DevTools/CSSTransitions/context/TransitionReducer.js
@@ -2,18 +2,20 @@ import { useReducer } from "react";
 import { MainCodeTemplates, PseudoCodeTemplates } from "./CodeTemplates";
 import { TransitionContext } from "./TransitionContext";
 
-const CHANGECODE = "CHANGECODE";
+const INITCODE = "INITCODE";
 const CHANGETYPE = "CHANGETYPE";
-const CHANGEPROPS = "CHANGEPROPS";
 const CHANGEDURATION = "CHANGEDURATION";
 const CHANGEDUNIT = "CHANGEDUNIT";
 const CHANGETFUNCTION = "CHANGETFUNCTION";
 const CHANGEDELAY = "CHANGEDELAY";
 const CHANGEDLUNIT = "CHANGEDLUNIT";
+const CHANGEMAINCODE = "CHANGEMAINCODE";
+const CHANGEPSEUDOCODE = "CHANGEPSEUDOCODE";
 
 const initialState = {
-  mainCode: `${MainCodeTemplates.opacity}\ntransition: opacity 1s ease 0s`,
-  pseudoCode: PseudoCodeTemplates.opacity,
+  mainCode: "",
+  pseudoCode: "",
+  transitionCode: "",
   transitionType: "opacity",
   duration: 1,
   durationUnit: "s",
@@ -25,41 +27,57 @@ const initialState = {
 const transitionReducer = (state, action) => {
   var mainCode = "";
   var pseudoCode = "";
+  var tCode = "";
   switch (action.type) {
-    case CHANGECODE:
-      mainCode = `
-      ${MainCodeTemplates[state.transitionType]};
-      ${"\n"}
-      transition: ${state.transitionType} ${state.duration}${
-        state.durationUnit
-      } ${state.timingFunction} ${state.delay}${state.delayUnit};
-      `;
+    case INITCODE:
+      mainCode = `${MainCodeTemplates[state.transitionType]}`;
+      tCode = `transition: ${state.transitionType} ${state.duration}${state.durationUnit} ${state.timingFunction} ${state.delay}${state.delayUnit};`;
       pseudoCode = `${PseudoCodeTemplates[state.transitionType]}`;
-
       return {
         ...state,
         mainCode: mainCode,
         pseudoCode: pseudoCode,
+        transitionCode: tCode,
+      };
+    case CHANGEMAINCODE:
+      mainCode = action.mainCode;
+      return {
+        ...state,
+        mainCode: mainCode,
+      };
+    case CHANGEPSEUDOCODE:
+      pseudoCode = action.pseudoCode;
+      return {
+        ...state,
+        pseudoCode: action.pseudoCode,
       };
     case CHANGETYPE:
+      tCode = `\ntransition: ${action.transitionType} ${state.duration}${state.durationUnit} ${state.timingFunction} ${state.delay}${state.delayUnit};`;
       return {
         ...state,
         transitionType: action.transitionType,
+        transitionCode: tCode,
       };
     case CHANGETFUNCTION:
+      tCode = `\ntransition: ${state.transitionType} ${state.duration}${state.durationUnit} ${action.tfvalue} ${state.delay}${state.delayUnit};`;
       return {
         ...state,
         timingFunction: action.tfvalue,
+        transitionCode: tCode,
       };
     case CHANGEDURATION:
+      tCode = `\ntransition: ${state.transitionType} ${action.duration}${state.durationUnit} ${state.timingFunction} ${state.delay}${state.delayUnit};`;
       return {
         ...state,
         duration: action.duration,
+        transitionCode: tCode,
       };
     case CHANGEDELAY:
+      tCode = `\ntransition: ${state.transitionType} ${state.duration}${state.durationUnit} ${state.timingFunction} ${action.delay}${state.delayUnit};`;
       return {
         ...state,
         delay: action.delay,
+        transitionCode: tCode,
       };
   }
 };

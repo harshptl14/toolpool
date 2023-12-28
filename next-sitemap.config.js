bc4ff66c9f0ba209cfd9cd0132ff53900aa0620e
arshptl <arshpatel714@gmail.com>
2023-12-28 12:46:12 -0500

    add: sitemap plugin

---
+++ next-sitemap.config.js
@@ -0,0 +1,9 @@
+const siteUrl = "https://www.toolpool.cool";
+
+module.exports = {
+  siteUrl,
+  generateRobotsTxt: true,
+  robotsTxtOptions: {
+    policies: [{ userAgent: "*", allow: "/" }],
+  },
+};
